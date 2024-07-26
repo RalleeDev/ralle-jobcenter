9b783909f4c4e6e4b6f6e7ac72e1c882fdda63b5
RalleeDev <109039505+RalleeDev@users.noreply.github.com>
2024-07-27 00:42:47 +0200

    feat(blip): Added blip and GetJob
    GetJob is not done as of right now

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,14 @@ fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
 
-server_script 'server/server.lua'
+author 'RalleeDev'
+Description 'A simple, low effort jobcenter for ox_core using ox_lib'
+version '0.0.5'
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/server.lua'
+}
 
 client_script 'client/client.lua'
 
--- server/server.lua
+++ server/server.lua
@@ -1,10 +1,17 @@
-lib.locale()
+lib.locale() --locale support
 
 RegisterNetEvent('ralle-jobcenter:Setjob', function (job, job_grade)
     local oxPlayer = Ox.GetPlayerFromUserId(source)
     local distance = #(Config.location - oxPlayer.getCoords())
     if distance < 1.5 then
         oxPlayer.setGroup(job, job_grade)
+        oxPlayer.setActiveGroup(job)
+        TriggerClientEvent('ox_lib:notify', source, {
+            id = 'ralle-jobcenter:newJob',
+            description = locale('congratulations'),
+            duration = 5000,
+            type = 'success'
+        })
     else
         TriggerClientEvent('ox_lib:notify', source, {
             id = 'ralle-jobcenter:error', 
@@ -15,9 +22,25 @@ RegisterNetEvent('ralle-jobcenter:Setjob', function (job, job_grade)
     end
 end)
 
+-- Doens't work atm
 lib.addCommand('jobs', {
     help = locale('job_command_help'),
 }, function(source, args, raw)
     local player = Ox.GetPlayerFromUserId(source)
-    local groups = player.getGroups()
+    GetJobs(player.charId)
 end)
+
+function GetJobs(charId)
+    local response = MySQL.query.await('SELECT `name`, `isActive` FROM `character_groups` WHERE `charId` = ?', {
+        charId
+    })
+    print(json.encode(response))
+
+    for _, row in ipairs(response) do
+        exports.chat:addMessage(-1, {
+            color = {255, 0, 0},
+            args = {"SYSTEM", string.format("Your current job is: ", row.name)}
+        })
+    end
+
+end
