89d9cfd458a0af7199ea437d87a07304831bc39c
RalleeDev <109039505+RalleeDev@users.noreply.github.com.>
2024-07-26 17:40:41 +0200

    chore(structure): Client/Server moved.
    Client and server scripts moved into folder aswell as updating fxmanifest.lua to match the new locations

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,9 +2,9 @@ fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
 
-server_script 'server.lua'
+server_script 'server/server.lua'
 
-client_script 'client.lua'
+client_script 'client/client.lua'
 
 shared_scripts {
     '@ox_lib/init.lua',
